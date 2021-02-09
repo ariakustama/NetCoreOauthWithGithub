8cea68ee117d8b9f648d019bb40d4fbad8de324d
ariakustama <aria.kustama@gmail.com>
2021-02-09 17:31:22 +0700

    First Commit

---
+++ NetCoreOauthGithub/Controller/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+
+namespace NetCoreOauthGithub.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login(string returnUrl = "/")
+        {
+            return Challenge(new AuthenticationProperties() { RedirectUri = returnUrl });
+        }
+    }
+}
---
+++ NetCoreOauthGithub/Startup.cs
@@ -0,0 +1,90 @@
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text.Json;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json.Linq;
+
+namespace NetCoreOauthGithub
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = "GitHub";
+            })
+               .AddCookie()
+               .AddOAuth("GitHub", options =>
+               {
+                   options.ClientId = Configuration["GitHub:ClientId"];
+                   options.ClientSecret = Configuration["GitHub:ClientSecret"];
+                   options.CallbackPath = new PathString("/github-oauth");
+                   options.AuthorizationEndpoint = "https://github.com/login/oauth/authorize";
+                   options.TokenEndpoint = "https://github.com/login/oauth/access_token";
+                   options.UserInformationEndpoint = "https://api.github.com/user";
+                   options.ClaimsIssuer = "OAuth2-Github";
+                   options.SaveTokens = true;
+                   options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
+                   options.ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
+                   options.ClaimActions.MapJsonKey("urn:github:login", "login");
+                   options.ClaimActions.MapJsonKey("urn:github:url", "html_url");
+                   options.ClaimActions.MapJsonKey("urn:github:avatar", "avatar_url");
+                   options.Events = new OAuthEvents
+                   {
+                       OnCreatingTicket = async context =>
+                       {
+                           var request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
+                           request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                           request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
+                           var response = await context.Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
+                           response.EnsureSuccessStatusCode();
+                           var json = JsonDocument.Parse(await response.Content.ReadAsStringAsync());
+                           context.RunClaimActions(json.RootElement);
+                       }
+                   };
+               });
+        }
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
